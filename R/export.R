38014201f81c4e7bd10494c96930e13da87a49ba
Daniel Falbel <dfalbel@gmail.com>
2022-01-25 10:00:09 -0300

    start allowing generating the C wrapper.

--- R/export.R
+++ R/export.R
@@ -43,11 +43,11 @@ generate_decls <- function(parsed) {
 make_error_handled <- function(parsed) {
   glue_code("
 <<make_declaration(parsed, macro = '', prefix = '')>>;
-<<make_declaration(parsed)>> {
+<<make_declaration(parsed, type = 'c_style')>> {
   try {
-    <<make_return(parsed)>> <<parsed$name>>(<<make_call(parsed)>>);
+    <<make_return(parsed)>> <<make_raw(parsed)>>(<<parsed$name>>(<<make_call(parsed, type = 'lantern')>>));
   } <<get_package_name()>>_HANDLE_EXCEPTION
-  <<if (parsed$return_type != 'void') paste('return (',parsed$return_type,') NULL')>>;
+  <<if (make_ret_type(parsed, 'c_style') != 'void') paste('return (',make_ret_type(parsed, 'c_style'),') NULL')>>;
 }
 ")
 }
@@ -71,25 +71,71 @@ glue_code <- function(..., .envir = parent.frame()) {
   glue::glue(..., .open = "<<", .close = ">>", .envir = .envir)
 }
 
-make_declaration <- function(parsed, prefix = "_", macro = NULL, inline = FALSE) {
+make_declaration <- function(parsed, prefix = "_", macro = NULL, inline = FALSE,
+                             type = c("unchanged", "c_style", "rcpp")) {
+  type <- match.arg(type)
   if (is.null(macro)) {
     macro <- glue_code("<<get_package_name()>>_API ")
   } else {
     macro <- macro
   }
   inline <- ifelse(inline, "inline ", "")
-  glue_code("<<macro>><<inline>><<parsed$return_type>> <<prefix>><<parsed$name>> (<<make_signature(parsed)>>)")
+  glue_code("<<macro>><<inline>><<make_ret_type(parsed, type = type)>> <<prefix>><<parsed$name>> (<<make_signature(parsed, type = type)>>)")
 }
 
-make_signature <- function(parsed) {
+make_signature <- function(parsed, type = c("unchanged", "c_style", "rcpp")) {
+  type <- match.arg(type)
   args <- parsed$args[[1]]
-  args <- paste(args$type, args$name)
+  types <- args$type
+  if (type == "c_style") {
+    types <- make_c_types(types)
+  }
+  args <- paste(types, args$name)
   paste(args, collapse = ", ")
 }
 
-make_call <- function(parsed) {
+make_ret_type <- function(parsed, type = c("unchanged", "c_style", "rcpp")) {
+  type <- match.arg(type)
+  if (type == "c_style")
+    make_c_types(parsed$return_type)
+  else
+    parsed$return_type
+}
+
+make_c_types <- function(types) {
+  types <- ifelse(types == "torch::Tensor", "void*", types)
+  types <- ifelse(types == "std::vector<torch::Tensor>", "void*", types)
+  types
+}
+
+make_lantern_type <- function(types) {
+  types <- ifelse(types == "torch::Tensor", "Tensor", types)
+  types <- ifelse(types == "std::vector<torch::Tensor>", "TensorList", types)
+  types
+}
+
+lantern_supported_types <- function() {
+  c("torch::Tensor", "std::vector<torch::Tensor>")
+}
+
+make_rcpp_type <- function(types) {
+  types <- ifelse(types == "torch::Tensor", "torch::Tensor", types)
+  types <- ifelse(types == "std::vector<torch::Tensor>", "torch::TensorList", types)
+  types
+}
+
+make_call <- function(parsed, type = c("unchanged", "lantern", "rcpp")) {
+  type <- match.arg(type)
   args <- parsed$args[[1]]
-  paste(args$name, collapse = ", ")
+  names <- args$name
+  types <- args$type
+  if (type == "lantern") {
+    # we can only add from_raw when the type is supported
+    names <- ifelse(types %in% lantern_supported_types(),
+                    paste0("from_raw::", make_lantern_type(types), "(", names, ")"),
+                    names)
+  }
+  paste(names, collapse = ", ")
 }
 
 make_return <- function(parsed) {
@@ -97,4 +143,12 @@ make_return <- function(parsed) {
     ""
   else
     "return "
+}
+
+make_raw <- function(parsed) {
+  if (parsed$return_type %in% c("torch::Tensor", "std::vector<torch::Tensor>")) {
+    paste0("make_raw::", make_lantern_type(parsed$return_type))
+  } else {
+    ""
+  }
 }
